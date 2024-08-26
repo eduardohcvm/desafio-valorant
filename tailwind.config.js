fce55f809448b4b706fe03f0ceecb8437bbce9c4
Eduardo Henrique Calado Vieira de Melo <57454262+eduardohcvm@users.noreply.github.com>
2024-08-26 16:46:22 -0300

    arquivos que estavam faltando para o tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Certifique-se de que isso cobre todos os arquivos que contêm classes Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
